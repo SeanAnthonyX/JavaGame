63c58796f6dca6a1de62b8bd7f20f86640841508
Sean A Stanley <47479498+SeanAnthonyX@users.noreply.github.com>
2019-06-27 16:08:06 -0400

    Add files via upload

---
+++ JavaGame/src/Game.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable{
+
+    public synchronized void start(){
+
+    }
+
+    public void run(){
+
+    }
+
+    public static void main (String [] args){
+
+    }
+}

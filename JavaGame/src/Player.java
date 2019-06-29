1f30962607906736a8a9e35af3f9608cba533250
Sean A Stanley <47479498+SeanAnthonyX@users.noreply.github.com>
2019-06-29 09:31:03 -0400

    Add files via upload

---
+++ JavaGame/src/Player.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.util.Random;
+
+public class Player extends GameObject {
+
+    Random rand = new Random();
+
+    public Player(int x, int y, ID id){
+        super(x, y, id);
+        velX = rand.nextInt(5) + 1;
+        velY = rand.nextInt(5) + 1;
+    }
+
+    public void tick(){
+
+        x += velX;
+        y += velY;
+    }
+
+    public void render(Graphics g){
+        g.setColor(Color.white);
+        g.fillRect(x,y,32,32);
+
+    }
+}
--- JavaGame/src/Window.java
+++ JavaGame/src/Window.java
@@ -5,18 +5,18 @@ import java.io.Serializable;
 public class Window extends Canvas {
 
     public Window(int width, int height, String title, Game game){
-        JFrame frame = new JFrame(title);
+        JFrame frame = new JFrame(title); // frame of window
 
         frame.setPreferredSize(new Dimension(width, height));
         frame.setMaximumSize(new Dimension(width, height));
-        frame.setMinimumSize((new Dimension(width, height));
+        frame.setMinimumSize(new Dimension(width, height));
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // stops program completely upon exiting
+        frame.setResizable(false); // do not allow user to change size of window
+        frame.setLocationRelativeTo(null); // starting position of window is middle of screen
         frame.add(game);
         frame.setVisible(true);
-        game.start();
+        game.start(); // created method
     }
 
 
